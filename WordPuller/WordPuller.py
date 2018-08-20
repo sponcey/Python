0f7c653e69f861fbe89887715848e8c789887f45
sponcey <spencer@charlesandthorn.com>
2018-08-19 23:00:24 -0400

    Initiated WordPuller

---
+++ WordPuller/WordPuller.py
@@ -0,0 +1,7 @@
+# From a dictionary (list) of words (strings), search through the list, and randomly pull a word of determined length (i.e. 5 characters).
+
+
+
+# From a dictionary (list) of words (strings), search through the list, and randomly pull words of cascading length (str.leng()) (10, 9, 8, 7, ...)
+
+	
