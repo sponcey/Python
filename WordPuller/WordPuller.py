dde49b566e2c800742b9c4968b5c6e9fdfd6645d
sponcey <spencer@charlesandthorn.com>
2018-08-21 20:21:53 -0400

    chaanges

--- WordPuller/WordPuller.py
+++ WordPuller/WordPuller.py
@@ -60,19 +60,17 @@ def wordlength(length):
         if len(random_word) == length:
             print(random_word)
             break 
-            
-wordlength(4) 
     
 # Now that the word length function is defined, nest that function in another function that iterates across a range of word lengths.
 
-# def wordlength_range(hichar, lowchar):
-#     while True:
-#         wordlength(hichar)
-#         if hichar != lowchar:
-#             hichar = hichar-1
-#         if hichar == lowchar:
-#             break
+def wordlength_range(hichar, lowchar):
+    while True:
+        wordlength(hichar)
+        if hichar != lowchar:
+            hichar = hichar-1
+        if hichar == lowchar:
+            break
             
-# wordlength_range(50,0)
+wordlength_range(10,0)
     
 # Mission Accomplished!!
