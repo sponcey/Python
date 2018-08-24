f9df5913aeddac450025ef50a7a2b600af349d95
sponcey <spencer@charlesandthorn.com>
2018-08-24 01:14:44 -0400

    Figured out WordPuller!
    Yayy

---
+++ WordPuller/WordCascader.py
@@ -0,0 +1,95 @@
+## WordCascader creates a function that, using three inputs (list, word width in units, number of words) generates a lists of perfectly justified words when set in the currently opened font. Pretty nifty! Much of the code is taken from Word-o-mat by Nina Stoessinger. <3
+
+from __future__ import print_function
+import random
+
+# Import dictionary as list
+text = open('lib/ukacd.txt', 'r')
+content = text.read()
+text.close()
+list(content)
+
+content_list = content.split('\n')
+del content_list[0:25]
+
+content_list_caps = []
+for word in content_list:
+    content_list_caps.append(word.upper())
+    
+content_list_sc = []
+for word in content_list:
+    content_list_sc.append(word.capitalize())
+    
+f = CurrentFont()
+
+#Check which unicode characters are available in the font.
+def fontCharacters(font):
+    if not font:
+        return []
+    charset = []
+    gnames = []
+    for g in font:
+        if g.unicode is not None:
+            try:
+                charset.append(unichr(int(g.unicode)))
+                gnames.append(g.name)
+            except ValueError:
+                pass
+    return charset, gnames
+        
+# # Helper function to find kerning between two given glyphs.
+# # This assumes MetricsMachine style group names.
+# def findKerning(chars):
+#     markers = ["@MMK_L_", "@MMK_R_"]
+#     keys = [c for c in chars]
+
+#     for i in range(2):
+#         allGroups = f.groups.findGlyph(chars[i])
+#         if len(allGroups) > 0:
+#             for g in allGroups:
+#                 if markers[i] in g:
+#                     keys[i] = g
+#                     continue
+
+#     key = (keys[0], keys[1])
+#     if f.kerning.has_key(key):
+#         return f.kerning[key]
+#     else:
+#         return 0
+
+# Function taken from Word-o-mat to look at words widths
+def sortWordsByWidth(wordlist, units, prints):
+    wordWidths = []
+    if f is not None:
+        fontChars, glyphNames = fontCharacters(f)
+        glyphNamesForValues = {fontChars[i]: glyphNames[i] for i in range(len(fontChars))}
+    else:
+        fontChars = []
+        glyphNames = []
+            
+    for word in wordlist:
+        unitCount = 0
+        for char in word:
+            try:
+                glyphWidth = f[char].width
+            except:
+                try:
+                    gname = glyphNamesForValues[char]
+                    glyphWidth = f[gname].width
+                except:
+                    glyphWidth = 0
+            unitCount += glyphWidth
+        # # add kerning
+        # for i in range(len(word)-1):
+        #     pair = list(word[i:i+2])
+        #     unitCount += int(findKerning(pair))
+        if units-50 <= unitCount <= units+50:
+            wordWidths.append(word)
+    while True:
+        print(random.choice(wordWidths))
+        if prints != 0:
+            prints = prints-1
+        if prints == 0:
+            break
+    
+sortWordsByWidth(content_list_caps, 4000, 10)
--- WordPuller/WordPuller.py
+++ WordPuller/WordPuller.py
@@ -7,70 +7,34 @@ import random
 
 # Open text file for reading
 f = open('lib/ukacd.txt', 'r')
-
-# Store content in a variable
 content = f.read()
-
-# close the file
 f.close()
-
-# # Count the number of spaces in content
-# def char_frequency(str1, char):
-#     count = 0
-#     for n in str1:
-#         if n == char:
-#             count += 1
-#     return count
-
-# print(char_frequency(content, '\n'))
 
 # Make list from content, using character breaks to divide list, and deleting all content up to first word in content
 list(content)
-
 content_list = content.split('\n')
-
 del content_list[0:25]
 
-# convert each string in the list to an integer representing the length of that string
-# def converttolength(listy):
-#     for n in listy[:]:
-#         len(n) = n
-        
-# content_list_length = converttolength(content_list)
+content_list_caps = []
+for word in content_list:
+    content_list_caps.append(word.upper())
 
-# if random word is a certain length, print it. if it isn't that length, keep looking until you find a random word of that length, then stop.
-## Problem: If user gives a length of a word that can't be found in the dictionary, Python will never be able to stop!!
-
+# wordlength function defines a list made up of words of a specified length.
 def wordlength(length):
-    # #first check if length is even in dictionary!
-    # ## how do I write this to stop looping once it's read every string?
-    # while True: 
-    #     for n in content_list[:]:
-    #         if len(n) != length:
-    #             return
-    #         elif len(n) == length:
-    #             continue
-    #         elif len(n) != length:
-    #             print("Couldn't find anything")
-    #             break
-    while True:
-        # defining variable for random word
-        random_word = random.choice(content_list)
-        ## this is pretty much exactly the same as above! lol
-        # for n in content_list:
-        #     pickedword = len(n)
-        #     if length != pickedword:
-        #         return
-        #     if length == pickedword:
-        #         print("List contains word of this lenghth!")
-        #         continue
-        # since we have established there is a word of that legnth, print one of them!
-        if len(random_word) == length:
-            print(random_word)
-            break 
+    # first, define a list that fulfills the desired attribute of length
+    outputwords = []
+    # for every word in the list, if the word matches the length, append it to the list
+    for w in content_list_caps:
+        if len(w) == length:
+            outputwords.append(w)
+    # if there are no words in the list, let me know!
+    if len(outputwords) == 0:
+        print("Can't find any words that length!")
+    # otherwise, randomly select a word from the new list
+    else:
+        print(random.choice(outputwords))          
 
 # Now that the word length function is defined, nest that function in another function that iterates across a range of word lengths.
-
 def wordlength_range(hichar, lowchar):
     while True:
         wordlength(hichar)
@@ -79,6 +43,6 @@ def wordlength_range(hichar, lowchar):
         if hichar == lowchar:
             break
             
-wordlength_range(15,3)
+wordlength_range(100,3)
     
 # Mission Accomplished!!
