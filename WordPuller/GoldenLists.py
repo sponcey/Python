59ce4f28e0fc28b29536c17efc0c2e88e255dd22
sponcey <spencer@charlesandthorn.com>
2018-08-30 12:14:29 -0400

    small changes

--- WordPuller/GoldenLists.py
+++ WordPuller/GoldenLists.py
@@ -7,27 +7,13 @@ text = open('lib/ukacd.txt', 'r', encoding='latin1')
 content = text.read()
 text.close()
 list(content)
-
-# # If external dict exists, read it and convert to dict.
-# if 
-#     text = open('lib/ukacd.txt', 'r')
-#     content = text.read()
-#     text.close()
-#     list(content)
-#     WordWidthDic = {}
-
-# Define Lists
 content_list = content.split('\n')
 del content_list[0:25]
 
-content_list_caps = []
-for word in content_list:
-    content_list_caps.append(word.upper())
-    
-content_list_sc = []
-for word in content_list:
-    content_list_sc.append(word.capitalize())
+# Path to Dictionary
+goldendict_look = Path("lib/goldendict.txt")
 
+# Function that defines list of characters in font
 def fontCharacters(font):
     if not font:
         return []
@@ -42,44 +28,53 @@ def fontCharacters(font):
                 pass
     return charset, gnames
 
-# Function that takes the word list, and compiles a dictionary by first adding the word; then adding a list (or tuple?) of values 
-# based on the measured glyphWidth of lowercase, uppercase, and sentence case.
-    ## Something isn't working!!   
-
-def MakeDic(wordlist):
-    if f is not None:
-        fontChars, glyphNames = fontCharacters(f)
-        glyphNamesForValues = {fontChars[i]: glyphNames[i] for i in range(len(fontChars))}
-    else:
-        fontChars = []
-        glyphNames = []
+# If external dict exists, read it and convert to dict.
+if goldendict_look.exists():
+    text = open('lib/goldendict.txt', encoding='latin1')
+    content = text.read()
+    text.close()
+    list(content)
+    WordWidthDic = {}
+# If external dict doesn't exist, make one!
+else:
+    goldendict = {}
+    # Function that takes the word list, and compiles a dictionary by first adding the word; then adding a list (or tuple?) of values 
+    # based on the measured glyphWidth of lowercase, uppercase, and sentence case.
+        ## Something isn't working!!   
+    def MakeDic(wordlist):
+        if f is not None:
+            fontChars, glyphNames = fontCharacters(f)
+            glyphNamesForValues = {fontChars[i]: glyphNames[i] for i in range(len(fontChars))}
+        else:
+            fontChars = []
+            glyphNames = []
      
-    # For every word in the list, measure its caps width, lower width, and sentence case list, and make that a list 
-    for word in wordlist:
-        capsword = word.upper()
-        lowerword = word.lower()
-        scaseword = word.upper()
-        WordLen = []
+        # For every word in the list, measure its caps width, lower width, and sentence case list, and make that a list 
+        for word in wordlist:
+            capsword = word.upper()
+            lowerword = word.lower()
+            scaseword = word.upper()
+            WordLen = []
             
-        def WordWidth(casedword):
-            unitCount = 0
-            for char in casedword:
-                try:
-                    glyphWidth = f[char].width
-                except:
+            def WordWidth(casedword):
+                unitCount = 0
+                for char in casedword:
                     try:
-                        gname = glyphNamesForValues[char]
-                        glyphWidth = f[gname].width
+                        glyphWidth = f[char].width
                     except:
-                        glyphWidth = 0
-                unitCount += glyphWidth
-            WordLen.append(unitCount)
-        WordWidth(capsword)
-        WordWidth(lowerword)
-        WordWidth(scaseword)
-        # Put that list and its corresponding word into the WordWidthDic
-        WordWidthDic.update({word: WordLen})
-        
+                        try:
+                            gname = glyphNamesForValues[char]
+                            glyphWidth = f[gname].width
+                        except:
+                            glyphWidth = 0
+                    unitCount += glyphWidth
+                WordLen.append(unitCount)
+            WordWidth(capsword)
+            WordWidth(lowerword)
+            WordWidth(scaseword)
+            # Put that list and its corresponding word into the WordWidthDic
+            WordWidthDic.update({word: WordLen})
+    MakeDic(content_list)
 # Biggest problem: Script is taking FOREVER 
 
 # Current Script: Go through and measuring the length of ALL WORDS, and make a list of words that are the desired LENGTH.
