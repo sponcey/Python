29370dfff65919af66d00685a3ccc9fe241a869b
Spencer Charles <spencer@charlesandthorn.com>
2018-08-31 18:24:21 -0400

    testing dictionary output/input

--- WordPuller/GoldenLists.py
+++ WordPuller/GoldenLists.py
@@ -1,4 +1,6 @@
-import random
+import random, json, os, time
+from pathlib import Path
+
 
 f = CurrentFont()
 
@@ -11,7 +13,8 @@ content_list = content.split('\n')
 del content_list[0:25]
 
 # Path to Dictionary
-goldendict_look = Path("lib/goldendict.txt")
+goldendict_look = Path('lib/tempdict.txt')
+goldendict = {1:2, 2:3}
 
 # Function that defines list of characters in font
 def fontCharacters(font):
@@ -28,64 +31,57 @@ def fontCharacters(font):
                 pass
     return charset, gnames
 
-# If external dict exists, read it and convert to dict.
+# If external dict exists, save the font, 
 if goldendict_look.exists():
-    goldendict = {}
-    with open('lib/goldendict.txt', encoding='latin1') as f:
-        for line in f:
-            (key, val) = line.split()
-            d[int(key)] = val
+    print("It exists!")
+    f.save()
+    ## If font was saved before generating new dict, use this dict.
+    # if os.path.getmtime(f.path) <= os.path.getmtime(goldendict_look):
+    with open('lib/tempdict.txt', 'r') as inf:
+        goldendict = eval(inf.read())
             
-# If external dict doesn't exist, make one!
-else:
-    goldendict = {}
-    # Function that takes the word list, and compiles a dictionary by first adding the word; then adding a list (or tuple?) of values 
-    # based on the measured glyphWidth of lowercase, uppercase, and sentence case.
-        ## Something isn't working!!   
-    def MakeDic(wordlist):
-        if f is not None:
-            fontChars, glyphNames = fontCharacters(f)
-            glyphNamesForValues = {fontChars[i]: glyphNames[i] for i in range(len(fontChars))}
-        else:
-            fontChars = []
-            glyphNames = []
-     
-        # For every word in the list, measure its caps width, lower width, and sentence case list, and make that a list 
-        for word in wordlist:
-            capsword = word.upper()
-            lowerword = word.lower()
-            scaseword = word.upper()
-            WordLen = []
-            
-            def WordWidth(casedword):
-                unitCount = 0
-                for char in casedword:
-                    try:
-                        glyphWidth = f[char].width
-                    except:
-                        try:
-                            gname = glyphNamesForValues[char]
-                            glyphWidth = f[gname].width
-                        except:
-                            glyphWidth = 0
-                    unitCount += glyphWidth
-                WordLen.append(unitCount)
-            WordWidth(capsword)
-            WordWidth(lowerword)
-            WordWidth(scaseword)
-            # Put that list and its corresponding word into the WordWidthDic
-            WordWidthDic.update({word: WordLen})
-    MakeDic(content_list)
-# Biggest problem: Script is taking FOREVER 
-
-# Current Script: Go through and measuring the length of ALL WORDS, and make a list of words that are the desired LENGTH.
-
-# Better Script: 
-# FIRST: check to see if a temporary folder /resources/ exists in the root of the script. 
-# If it does, continue. 
-# If it doesn't, create a temp folder.
-# SECOND: Check to see if a temp dictionary of word length values exists in /resources/. 
-# If it doesn't, generate a dictionary containing the lengths of the words, and export that dictionary to /resources/. then print "New Dictionary Generated!"
-# If it does, then:
-    # Check to see if the font has been modified since the dictionary was generated. If it was, overwrite the old dict, and print "Dict Overwritten!"
-    # If the font hasn't been modified since the dictionary was generated, then continue.
+# # If external dict doesn't fulfill the above, make one!
+# else:
+#     f.save()
+#     # Function that takes the word list, and compiles a dictionary by first adding the word; then adding a list (or tuple?) of values 
+#     # based on the measured glyphWidth of lowercase, uppercase, and sentence case.
+#     ## Something isn't working!!   
+#     if f is not None:
+#         fontChars, glyphNames = fontCharacters(f)
+#         glyphNamesForValues = {fontChars[i]: glyphNames[i] for i in range(len(fontChars))}
+#     else:
+#         fontChars = []
+#         glyphNames = []
+ 
+#     # For every word in the list, measure its caps width, lower width, and sentence case list, and make that a list 
+#     def DictGen(wordlist):
+#         for word in wordlist:
+#             capsword = word.upper()
+#             lowerword = word.lower()
+#             scaseword = word.upper()
+#             WordLen = []
+        
+#             def WordWidth(casedword):
+#                 unitCount = 0
+#                 for char in casedword:
+#                     try:
+#                         glyphWidth = f[char].width
+#                     except:
+#                         try:
+#                             gname = glyphNamesForValues[char]
+#                             glyphWidth = f[gname].width
+#                         except:
+#                             glyphWidth = 0
+#                     unitCount += glyphWidth
+#                 WordLen.append(unitCount)
+#             WordWidth(capsword)
+#             WordWidth(lowerword)
+#             WordWidth(scaseword)
+#             # Put that list and its corresponding word into the WordWidthDic
+#             goldendict.update({word: WordLen})
+#     DictGen(content_list)
+#     with open('lib/tempdict.txt', 'w') as tempdict:
+#         tempdict.write(json.dumps(goldendict))
+        
+print(goldendict[1])
+        
--- WordPuller/dicttest.py
+++ WordPuller/dicttest.py
@@ -1,9 +1,19 @@
-import pickle
+import json
 
-testdict = {'garbage': [10,5,6], 'eulogy': [11,6,7], 'frederick': [100,40,10]}
+testdict = {'garbage': [10,5,6], 'eulogy': [11,6,7], 'frederick': [100,40,30]}
 
-# # Prints the first item in the list from the value
-# for key,val in testdict.items():
-#     print(val[0])
+# Prints the first item in the list from the value
+for key,val in testdict.items():
+    print(val[0])
+
+# Writes the dictionary to file    
+with open('lib/tempdict.txt', 'w') as tempdict:
+    tempdict.write(json.dumps(testdict))
     
-print(testdict)
+# Reads the dictionary file back and converts to dictionary in python.
+with open('lib/tempdict.txt', 'r') as inf:
+    dict_from_file = eval(inf.read())
+
+
+for key,val in dict_from_file.items():
+    print(val[2])
